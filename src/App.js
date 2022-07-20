054f050a7807af0500486b6f8d43b51cdf333947
Carlos Silva <carlos.silvap@outlook.com>
2022-07-20 16:49:27 -0500

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
       <h2>Online Store</h2>
     </div>
   );
